5020ddca163932167f7ed0157c1aca0fa0e6253f
Hector Gabriel Meneses A <hector.meneses@globant.com>
2024-02-21 21:44:55 -0500

    input events example

---
+++ input-events/src/App.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+  state = {
+    text: '',
+    evento: '',
+  }
+
+  manejador = (event) => {
+    this.setState({
+      text: event.target.value,
+      evento: event.type,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type={'text'}
+          onChange={this.manejador}
+          onCopy={this.manejador}
+          onPaste={this.manejador}
+        />
+        <h1>{this.state.text}</h1>
+        <h2>{this.state.evento}</h2>
+      </div>
+    )
+  }
+}
+
+export default App
